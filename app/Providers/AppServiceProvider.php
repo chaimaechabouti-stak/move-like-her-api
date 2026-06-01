d0135ef467796d859148318ce810cb7ecd96285e
Chaimae <chaimaechabouti@gamil.com>
2026-06-01 02:19:37 +0100

    fix: override migrate command on vercel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,5 +20,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        if (env('VERCEL') || env('NOW_REGION')) {
+        \Illuminate\Support\Facades\Artisan::command('migrate --force', function () {
+            $this->info('Migrations bypassées sur Vercel pour protéger vos données phpMyAdmin !');
+        });
+    }
     }
 }
